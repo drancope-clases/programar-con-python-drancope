1f7c0d52109fdb945e79bfb7d287cee883142e8f
Alvaro8976 <92920770+Alvaro8976@users.noreply.github.com>
2021-10-28 10:23:02 +0000

    ejercicio de multiplicar

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("Hola")
 # Este repositorio tiene una rama por cada estudiante. Esta rama es la de ÁLvaro
 # a ver si funciona esta vez
+numero = 9 #pon aquí el número del que quieres hacer la tabla
+print("tabla de multiplicar del ", numero)
+for i in range(10):
+ print(numero, "por", i," = ", i*numero)
